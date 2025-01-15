d1e6bba29c814543cef5ac6c819abf212f8e2bb6
thiagopaivamed <thiagopaivamed@gmail.com>
2025-01-15 18:50:12 -0300

    Restaurant class created

---
+++ SUperDInner.Domain/Entities/IBaseEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace SuperDinner.Domain.Entities
+{
+    public interface IBaseEntity
+    {
+        public DateTime CreatedDate { get; set; }
+
+        public DateTime? LastModifiedDate { get; set; }
+    }
+}
---
+++ SUperDInner.Domain/Entities/Restaurant.cs
@@ -0,0 +1,30 @@
+﻿namespace SuperDinner.Domain.Entities
+{
+    public sealed class Restaurant : IBaseEntity
+    {
+        public Guid RestaurantId { get; private set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public string ContactPhone { get; set; } = string.Empty;
+
+        public string Address { get; set; } = string.Empty;
+
+        public string Country { get; set; } = string.Empty;
+
+        public double Latitude { get; set; }
+
+        public double Longitude { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+        public DateTime? LastModifiedDate { get; set; }
+
+        public Restaurant()
+        {
+            RestaurantId = Guid.NewGuid();
+            CreatedDate = DateTime.UtcNow;
+        }
+    }
+}
