c2ef4cdadc648d6eb4b3161083bbc414486a1615
thiagopaivamed <thiagopaivamed@gmail.com>
2025-02-06 18:51:09 -0300

    Keep going

--- SuperDinner.UnitTests/obj/Debug/net9.0/SuperDinner.UnitTests.AssemblyInfo.cs
+++ SuperDinner.UnitTests/obj/Debug/net9.0/SuperDinner.UnitTests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SuperDinner.UnitTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a978b5359ac71354029005d9171cb28eb8a08760")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40484aba29283bd4918f494736eee58d7c6f28f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("SuperDinner.UnitTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SuperDinner.UnitTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
